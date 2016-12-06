92cf3b938565699a5fa5f55d346517b05300f0d6
elizheld <bizrfoot21@yahoo.com>
2016-12-06 17:41:13 -0600

    Update cc-cae-keras.py

--- cc-cae-keras.py
+++ cc-cae-keras.py
@@ -17,16 +17,23 @@ random.seed(14)
 
 input_img = Input(shape=(1, 8, 8))
 
-x = Convolution2D(2, 2, 2, activation='relu', border_mode='same')(input_img)
+x = Convolution2D(16, 2, 2, activation='relu', border_mode='same')(input_img)
 x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(8, 2, 2, activation='relu', border_mode='same')(x)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(8, 2, 2, activation='relu', border_mode='same')(x)
 encoded = MaxPooling2D((2, 2), border_mode='same')(x)
 
 # at this point the representation is (8, 4, 4) i.e. 128-dimensional
 
-
-x = Convolution2D(2, 2,2, activation='relu')(x)
+x = Convolution2D(8, 2, 2 activation='relu', border_mode='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(8, 2, 2, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(16, 2, 2, activation='relu')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Convolution2D(1, 4, 4, activation='sigmoid', border_mode='same')(x)
+
 
 digits = datasets.load_digits()
 target_names = digits.target_names
