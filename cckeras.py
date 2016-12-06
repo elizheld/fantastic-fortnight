2975d78f0057dac454679612b2104fccb442c060
elizheld <bizrfoot21@yahoo.com>
2016-12-06 15:52:31 -0600

    Update cckeras.py

--- cckeras.py
+++ cckeras.py
@@ -6,14 +6,14 @@ from keras.layers import Input, Dense
 from keras.models import Model
 
 # this is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+encoding_dim = 16  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
 # this is our input placeholder
-input_img = Input(shape=(784,))
+input_img = Input(shape=(64,))
 # "encoded" is the encoded representation of the input
 encoded = Dense(encoding_dim, activation='relu')(input_img)
 # "decoded" is the lossy reconstruction of the input
-decoded = Dense(784, activation='sigmoid')(encoded)
+decoded = Dense(64, activation='sigmoid')(encoded)
 autoencoder = Model(input=input_img, output=decoded)
 encoder = Model(input=input_img, output=encoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
