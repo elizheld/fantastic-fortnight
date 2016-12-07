4ddbe76be1d84ba8f42623b79b4bf6833e44fabb
elizheld <bizrfoot21@yahoo.com>
2016-12-07 13:51:58 -0600

    Update cc-keras-dae-MNIST.py

--- cc-keras-dae-MNIST.py
+++ cc-keras-dae-MNIST.py
@@ -23,12 +23,16 @@ encoding_dim = 64
 
 # this is our input placeholder
 input_img = Input(shape=(784,))
-encoded = Dense(128, activation='sigmoid')(input_img)
-encoded = Dense(16, activation='sigmoid')(encoded)
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
+encoded = Dense(16, activation='relu')(encoded)
 
-decoded = Dense(16, activation='sigmoid')(encoded)
-decoded = Dense(128, activation='sigmoid')(decoded)
-decoded = Dense(784, activation='sigmoid')(decoded)
+decoded = Dense(16, activation='relu')(encoded)
+decoded = Dense(32, activation='relu')(encoded)
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(784, activation='relu')(decoded)
 
 autoencoder = Model(input=input_img, output=decoded)
 encoder = Model(input=input_img, output=encoded)
@@ -68,7 +72,7 @@ X_test = X_test.reshape((len(X_test), np.prod(X_test.shape[1:])))
 
 #N Train our autoencoder for 100 epochs:
 autoencoder.fit(X_train, X_train,
-                nb_epoch=100,
+                nb_epoch=50,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(X_test, X_test))
