6e091e6dc965b95f2a1f4c2e8e2b61b377e8f1eb
elizheld <bizrfoot21@yahoo.com>
2016-12-06 21:56:39 -0600

    Update cc-dae-keras.py

--- cc-dae-keras.py
+++ cc-dae-keras.py
@@ -85,3 +85,17 @@ plt.ylabel('True Positive Rate')
 plt.title('Receiver operating characteristic After AE')
 plt.legend(loc="lower right")
 plt.show()
+
+def plot_gallery(images, h, w, n_row=3, n_col=4):
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray, interpolation='nearest')
+        plt.xticks(())
+        plt.yticks(())
+
+plot_gallery(X_test, h, w)
+plot_gallery(encoded_imgs, 4, 4)
+
+plt.show()
