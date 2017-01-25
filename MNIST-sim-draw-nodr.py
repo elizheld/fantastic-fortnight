e263812524f36afb84dc60e28d8e427a13092ed5
elizheld <bizrfoot21@yahoo.com>
2017-01-24 22:06:54 -0600

    Update MNIST-sim-draw-nodr.py

--- MNIST-sim-draw-nodr.py
+++ MNIST-sim-draw-nodr.py
@@ -37,8 +37,8 @@ X_test = np.array(X_test)
 # Get simulation indices
 n_train = N*len(X_train)
 n_test = N*len(X_test)
-ind_train = np.random.choice(np.array([0,1]), size=n_train, p=[0.67,0.33]).reshape(N, len(X_train))
-ind_test =  np.random.choice(np.array([0,1]), size=n_test, p=[0.67,0.33]).reshape(N, len(X_test))
+ind_train = np.random.choice(np.array([0,1]), size=n_train, p=[0.9,0.1]).reshape(N, len(X_train))
+ind_test =  np.random.choice(np.array([0,1]), size=n_test, p=[0.9,0.1]).reshape(N, len(X_test))
     
 # perform analyses for sae
 hold2 = [0]*N
